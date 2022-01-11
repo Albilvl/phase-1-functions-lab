15d5072bc601bd76a41cbe6998474a2aac7fce2d
Al Bilal <bil.bank@yahoo.com>
2022-01-11 09:50:58 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(somevalue){
+ return Math.abs(somevalue - 42) 
+}
+
+function distanceFromHqInFeet(somevalue){
+    return Math.abs(somevalue - 42) * 264
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(destination-start)*264
+}
+
+function calculatesFarePrice(start, destination){
+    let dist = distanceTravelledInFeet(start, destination)
+    if (dist> 2500){
+        return `cannot travel that far`
+    } else if( dist > 2000) {
+        return  25
+    } else if ( dist > 400){
+        let newdist = dist - 400
+        return newdist * 0.02
+    } else {
+        return 0
+    }
+    }
